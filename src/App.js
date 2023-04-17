7ac0a83dd4ef5b167f075489a2462389275bb666
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2023-04-17 14:59:22 +0900

    working...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Profile from './components/profile.js';
 import Contents from './components/contents.js'
+import About from './components/01about.js'
 import './App.css';
 
 function App() {
   return (
     <>
-      <Profile />
-      <Contents />
+      <div className='App'>
+        <Profile />
+        <Contents />
+        <About />
+      </div>
     </>
   );
 }
---
+++ src/components/01about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './01about.css';
+
+function About() {
+  return (
+    <>
+      <div className='about_container'>Hi</div>
+    </>
+  )
+}
+
+export default About;
