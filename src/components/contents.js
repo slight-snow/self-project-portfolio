04d652f479e7f23c6230159eb3821be8a43e1c00
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2023-04-17 12:11:51 +0900

    working...

--- src/components/contents.js
+++ src/components/contents.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './contents.css';
 
 function Contents() {
+  const [active, setActive] = useState(0);
+
+  console.log(active)
+
   return (
     <>
       <div>
         <div className='contents_container'>
           <div className='contents_box'>
-            <div className='contents_title'>
-              <div>About</div>
-              <div>Skill</div>
-              <div>Project</div>
-              <div>Contact</div>
-            </div>
-            <div className='contents_line'>
-              <div className='dots_container'>
-                <div className='unselected' />
-                <div className='unselected' />
-                <div className='unselected' />
-                <div className='unselected' />
+            <div className='contents_line_dots_container'>
+              <div className='contents_line'></div>
+              <div className='contents'>
+                <div className='contents_title'>About</div>
+                <div
+                  className='contents_dot_unselected'
+                  onClick={() => setActive(1)} />
+              </div>
+              <div className='contents'>
+                <div className='contents_title'>Skill</div>
+                <div
+                  className='contents_dot_unselected'
+                  onClick={() => setActive(2)}>
+                </div>
+              </div>
+              <div className='contents'>
+                <div className='contents_title'>Work</div>
+                <div
+                  className='contents_dot_unselected'
+                  onClick={() => setActive(3)} />
+              </div>
+              <div className='contents'>
+                <div className='contents_title'>Contact</div>
+                <div
+                  className='contents_dot_unselected'
+                  onClick={() => setActive(4)} />
               </div>
             </div>
           </div>
