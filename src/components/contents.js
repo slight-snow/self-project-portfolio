9b19eee7bc590d9a4e30fbb1af8d16bab1cddd3e
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2023-04-17 13:57:41 +0900

    working

--- src/components/contents.js
+++ src/components/contents.js
@@ -12,32 +12,53 @@ function Contents() {
         <div className='contents_container'>
           <div className='contents_box'>
             <div className='contents_line_dots_container'>
-              <div className='contents_line'></div>
-              <div className='contents'>
-                <div className='contents_title'>About</div>
-                <div
-                  className='contents_dot_unselected'
-                  onClick={() => setActive(1)} />
-              </div>
-              <div className='contents'>
-                <div className='contents_title'>Skill</div>
-                <div
-                  className='contents_dot_unselected'
-                  onClick={() => setActive(2)}>
-                </div>
-              </div>
-              <div className='contents'>
-                <div className='contents_title'>Work</div>
-                <div
-                  className='contents_dot_unselected'
-                  onClick={() => setActive(3)} />
-              </div>
-              <div className='contents'>
-                <div className='contents_title'>Contact</div>
-                <div
-                  className='contents_dot_unselected'
-                  onClick={() => setActive(4)} />
-              </div>
+              <div className='contents_line' />
+
+              {/* ABOUT */}
+              {active === 1 ?
+                <div className='contents'>
+                  <div className='contents_title_selected'>About</div>
+                  <div className='contents_dot_selected' />
+                </div> :
+                <div className='contents' onClick={() => setActive(1)}>
+                  <div className='contents_title_unselected'>About</div>
+                  <div className='contents_dot_unselected' />
+                </div>}
+
+              {/* SKILL */}
+              {active === 2 ?
+                <div className='contents'>
+                  <div className='contents_title_selected'>Skill</div>
+                  <div
+                    className='contents_dot_selected' />
+                </div> :
+                <div className='contents' onClick={() => setActive(2)}>
+                  <div className='contents_title_unselected'>Skill</div>
+                  <div className='contents_dot_unselected' />
+                </div>}
+
+              {/* WORK */}
+              {active === 3 ?
+                <div className='contents'>
+                  <div className='contents_title_selected'>Work</div>
+                  <div className='contents_dot_selected' />
+                </div> :
+                <div className='contents' onClick={() => setActive(3)}>
+                  <div className='contents_title_unselected'>Work</div>
+                  <div className='contents_dot_unselected' />
+                </div>}
+
+              {/* CONTACT */}
+              {active === 4 ?
+                <div className='contents'>
+                  <div className='contents_title_selected'>Contact</div>
+                  <div className='contents_dot_selected' />
+                </div> :
+                <div className='contents' onClick={() => setActive(4)}>
+                  <div className='contents_title_unselected'>Contact</div>
+                  <div className='contents_dot_unselected' />
+                </div>}
+
             </div>
           </div>
         </div>
