423e19149ffedbf32fb4d9e4dcc1b62f333605d3
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2023-04-27 00:21:39 +0900

    working...

--- src/components/04contact/04contact.js
+++ src/components/04contact/04contact.js
@@ -3,6 +3,21 @@ import { useDispatch, useSelector } from 'react-redux';
 import { page04 } from '../../features/counter/counterSlice';
 import './04contact.css';
 
+import emailjs from '@emailjs/browser';
+
+function sendEmail(event) {
+  event.preventDefault();
+
+  console.log(event.target)
+
+  emailjs.sendForm('service_fr1lhns', 'template_qku4pks', event.target, 'lz3cUWX4ve311rPtk')
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    })
+}
+
 function Contact() {
   const page = useSelector((state) => state.counter.page);
   const dispatch = useDispatch();
@@ -36,9 +51,16 @@ function Contact() {
           className='contact_container'
           onClick={() => dispatch(page04())}>
           <div className='contact_box'>
-            <div>04</div>
-            <div className='contact_break_line' />
-            <div className='contact_title'>CONTACT</div>
+            {/* <form className='contact_form' onSubmit={sendEmail}>
+              <input type='hidden' name='contact_number' />
+              <label>Name</label>
+              <input type='text' name='user_name' />
+              <label>Email</label>
+              <input type='email' name='user_email' />
+              <label>Text</label>
+              <textarea name='message' />
+              <input type='submit' value='Send' />
+            </form> */}
           </div>
         </div>
       </>
