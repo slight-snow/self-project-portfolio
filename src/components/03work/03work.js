b45dc3e25e9ac0de45a3a642f7bb1594b17f99e0
SEB-Full-33-Yunseok <forcoding97@gmail.com>
2023-05-13 23:02:42 +0900

    working...

--- src/components/03work/03work.js
+++ src/components/03work/03work.js
@@ -57,12 +57,24 @@ function Work() {
               <div className='work_script_box'>
                 <div className='work_script_title'>반띵(BanThing)</div>
                 <div className='work_script_content'>
-
+                  가까운 거리 내에 있는 주변 사용자들과 채팅방을 통해 의사소통하여
+                  <br />배달 음식에 대한 배달비를 분담할 수 있는 웹 어플리케이션입니다.
+                  <br />
+                  <br />카카오맵 API를 활용한 웹 어플리케이션으로,
+                  <br />사용자는 현재 자신의 위치를 기반으로 새로운 방을 생성하거나
+                  <br />기존에 생성되어 있는 주변의 방에 참가할 수 있습니다.
+                  <br />
+                  <br />배달시키고자 하는 음식과 역할을 설정하고
+                  <br />이를 바탕으로 사용자들 간의 세부 사항을 조율할 수 있습니다.
+                  <br />
+                  <br />최소주문금액, 배달팁 등 배달 음식에 대해
+                  <br />사용자들이 느끼는 부담을 해소하고자 개발된 팀 프로젝트입니다.
                 </div>
 
                 <div className='work_detail_box'>
-                  <div className='work_detail_01'></div>
-                  <div className='work_detail_02'></div>
+                  <ul className='work_detail_01'></ul>
+                  <li>hi</li>
+                  <ul className='work_detail_02'></ul>
                 </div>
               </div>
             </div>
